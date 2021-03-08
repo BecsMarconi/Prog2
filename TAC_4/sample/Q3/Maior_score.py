5f449b115da4fad6e463369dc593cc4ec143c272
BecsMarconi <75839045+BecsMarconi@users.noreply.github.com>
2021-03-08 17:40:04 -0300

    Update Maior_score.py

--- TAC_4/sample/Q3/Maior_score.py
+++ TAC_4/sample/Q3/Maior_score.py
@@ -1,29 +1,26 @@
 """
 Author: Rebeca Marconi Pereira Raboeira
 Date: 05/02/2021 - 18:03
-Version: 7?
-Last update: 06/03/2021 - 02:16
+Version: 8
+Last update: 08/03/2021 - 16:39
 """
-
+import pandas #teste
 from Bio.Blast.Applications import NcbiblastxCommandline
 
-#
 # C:/Users/rebec/PycharmProjects/Prog2/TAC_4/lib/sequenciaDesconhecida.fasta
 # C:/Users/rebec/PycharmProjects/Prog2/TAC_4/lib/TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta
 
-caminho_blast = "C:/Users/rebec/miniconda3/envs/Prog2/Lib/site-packages/Bio/Blast/NCBIXML.py"
-# por algum motivo a única coisa que se relaciona como blast está nessa pasta e mesmo apagando e reinstalando
-# biopython e cia pelo conda, tudo de novo surge nessa mesma pasta
+caminho_blast = r"C:\Program Files\NCBI\blast-2.11.0+\bin\blastx.exe"
 
-meuOutput = "out.blastp.outfmt6.txt"
+meuOutPut = "C:/Users/rebec/PycharmProjects/Prog2/TAC_4/lib/out.blastp.outfmt6.txt"
 
-refArquivoFasta = open(input("Insira o caminho (de forma correta) para o arquivo FASTA: "))
-refArquivoMultiFasta = open(input("Insira o caminho (de forma correta) para o arquivo multi-fasta de proteínas: "))
+refArquivoFasta = input("Insira o caminho (de forma correta) para o arquivo FASTA: ")
+refArquivoMultiFasta = input("Insira o caminho (de forma correta) para o arquivo multi-fasta de proteínas: ")
 
 comando_blast = NcbiblastxCommandline(query=refArquivoFasta,
                                       subject=refArquivoMultiFasta,
                                       outfmt=6,
-                                      out=meuOutput,
+                                      out=meuOutPut,
                                       evalue=0.05,
                                       cmd=caminho_blast)
 
@@ -33,7 +30,7 @@ stdout, stderr = comando_blast()
 
 print("Fim da busca local...")
 
-blast_resultado = open(meuOutput, "r")
+blast_resultado = open(meuOutPut, "r")
 
 qseqid = 0
 sseqid = 1
@@ -57,9 +54,7 @@ for linha in s:
 
     if len(hit) != 12:
         break
-
-    melhor_score = hit[bitscore][-1]
-    print("Maior Score: {}".format(melhor_score))
-
-refArquivoMultiFasta.close()
-refArquivoFasta.close()
+    
+    maior_score = blast_resultado.sort_values('bitscore')
+    print(maior_score.iloc[[-1]])
+    
